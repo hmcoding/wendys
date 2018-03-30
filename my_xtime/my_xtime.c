4495df17c3ec21cc33c33260e50fd1989cdeb476
hmcoding <32525204+hmcoding@users.noreply.github.com>
2018-03-29 23:32:03 -0400

    Update my_xtime.c

--- my_xtime/my_xtime.c
+++ my_xtime/my_xtime.c
@@ -6,7 +6,7 @@
 #include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Culver");
+MODULE_AUTHOR("Culver Perry Davis");
 MODULE_DESCRIPTION("Simple module featuring proc read");
 
 #define ENTRY_NAME "timed"
@@ -17,8 +17,8 @@ static struct file_operations fops;
 
 static char *message;
 static int read_p;
-static int prev_seconds;
-static int prev_nseconds;
+static int lastSec;
+static int lastNSec;
 
 int hello_proc_open(struct inode *sp_inode, struct file *sp_file) {
 	printk(KERN_INFO "proc called open\n");
@@ -35,32 +35,63 @@ int hello_proc_open(struct inode *sp_inode, struct file *sp_file) {
 ssize_t hello_proc_read(struct file *sp_file, char __user *buf, size_t size, loff_t *offset) {	
 	struct timespec time;
 	time = current_kernel_time();
-	if (prev_seconds == -1)
+	
+	if(lastSec != -1)
+	{
+		int elapsedSec;
+		int elapsedNSec;
+		// need to borrow
+		if (lastNSec > time.tv_nsec)
+		{
+			elapsedSec = time.tv_sec - lastSec - 1;
+			elapsedNSec = time.tv_nsec + 1000000000 - lastNSec;
+		}
+		else
+		{
+			elapsedSec = time.tv_sec - lastSec;
+			elapsedNSec = time.tv_nsec - lastNSec;
+		}
+		sprintf(message, "current time: %d.%d\nelapsed time: %d.%d\n", 
+			time.tv_sec, time.tv_nsec, 
+			elapsedSec, elapsedNSec);
+	}
+	else
+	{
+		sprintf(message, "current time: %d.%d\n", time.tv_sec, time.tv_nsec);
+	}
+	
+	
+	
+	
+	/*
+	if (lastSec == -1)
 	{
 		sprintf(message, "current time: %d.%d\n", time.tv_sec, time.tv_nsec);
 	}	
 	else
 	{
-		int deltaSeconds;
-		int deltaNSeconds;
+		int elapsedSec;
+		int elapsedNSec;
 		// need to borrow
-		if (prev_nseconds > time.tv_nsec)
+		if (lastNSec > time.tv_nsec)
 		{
-			deltaSeconds = time.tv_sec - prev_seconds - 1;
-			deltaNSeconds = time.tv_nsec + 1000000000 - prev_nseconds;
+			elapsedSec = time.tv_sec - lastSec - 1;
+			elapsedNSec = time.tv_nsec + 1000000000 - lastNSec;
 		}
 		else
 		{
-			deltaSeconds = time.tv_sec - prev_seconds;
-			deltaNSeconds = time.tv_nsec - prev_nseconds;
+			elapsedSec = time.tv_sec - lastSec;
+			elapsedNSec = time.tv_nsec - lastNSec;
 		}
 		sprintf(message, "current time: %d.%d\nelapsed time: %d.%d\n", 
 			time.tv_sec, time.tv_nsec, 
-			deltaSeconds, deltaNSeconds);
+			elapsedSec, elapsedNSec);
 	}
+	*/
+	
 	// store current time as old time
-	prev_seconds = time.tv_sec;
-	prev_nseconds = time.tv_nsec;
+	lastSec = time.tv_sec;
+	lastNSec = time.tv_nsec;
 
 	int len = strlen(message);
 	
@@ -88,8 +119,8 @@ static int hello_init(void) {
 	fops.read = hello_proc_read;
 	fops.release = hello_proc_release;
 	
-	prev_seconds = -1;
-	prev_nseconds = -1;
+	lastSec = -1;
+	lastNSec = -1;
 
 	if (!proc_create(ENTRY_NAME, PERMS, NULL, &fops)) {
 		printk(KERN_WARNING "proc_create\n");
@@ -99,7 +130,6 @@ static int hello_init(void) {
 	return 0;
 }
 
-//module_init(hello_init); // from his example
 
 static void hello_exit(void) {
 	remove_proc_entry(ENTRY_NAME, NULL);

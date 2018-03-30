08d369b736f99e65fa8cd7510aa4e809bf842c73
hmcoding <32525204+hmcoding@users.noreply.github.com>
2018-03-29 23:39:11 -0400

    Update syscalls.h

--- part3/include/syscalls.h
+++ part3/include/syscalls.h
@@ -3,24 +3,19 @@
 
 void elevator_syscalls_create(void);
 void elevator_syscalls_remove(void);
-void InitializeQueue(void);
-void CleanupQueue(void);
-void QueuePassenger(int type, int start, int dest);
-void PrintQueue(void);
-char* PrintQueueToString(void);
-void PrintElevator(void);
-int ElevatorRun(void* data);
-int MoveElevator(int floor);
-int PassengerQueueSize(int floor);
-int PassengerQueueWeight(int floor);
-int ElevatorListSize(void);
-void UnloadPassengers(void);
-int ShouldUnload(void);
-int ShouldLoad(void);
-int ElevatorWeight(void);
-
-// FIFO implementation functions
-int NextPickupFloor(void);
-int NextDropoffFloor(void);
-void FIFOLoadSinglePassenger(int floor);
+void qInitial(void);
+void qClean(void);
+void qPass(int type, int start, int dest);
+int elevGo(void* data);
+int elevChange(int floor);
+int sizeQPass(int floor);
+int weightQPass(int floor);
+int sizeElevList(void);
+void passOff(void);
+int Unload(void);
+int Load(void);
+int getElevWeight(void);
+int Pickup(void);
+int Dropoff(void);
+void loadOnePass(int floor);
 #endif /*__ELEVATOR_SYSCALLS_H*/
